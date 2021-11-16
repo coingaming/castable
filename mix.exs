f8791d470e45b750e65fea07b7810ca088dad723
Reio Piller <reio@heathmont.net>
2021-11-16 21:07:41 +0200

    publish

--- mix.exs
+++ mix.exs
@@ -7,7 +7,12 @@ defmodule Castable.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: "Protocol to aid casting unknown values to known value",
+      package: [
+        licenses: ["MIT"],
+        links: %{"GitHub" => "https://github.com/coingaming/castable"},
+      ]
     ]
   end
 
@@ -21,6 +26,7 @@ defmodule Castable.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 end
